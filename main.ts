d4f64bdfdab93eba16d7c445c06577c594858ab4
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-04-24 13:09:40 -0400

    change distance requirement

--- main.ts
+++ main.ts
@@ -1,8 +1,32 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Dat Nguyen
+ * Created on: Apr 2026
+ * This program will send "Too close!" to another micro:bit
 */
 
-basic.showString('Hello, World!')
+// initialize radio
+radio.setGroup(153)
+
+// initialize display
+basic.showIcon(IconNames.Happy)
+
+// initialize variables
+let distance: number = -1
+
+// initialize display
+basic.showIcon(IconNames.Happy)
+
+// handle button A press
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Triangle)
+    while (true) {
+        // calculate distance
+        distance = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Centimeters)
+
+        // send message to receiving micro:bit
+        if (distance > 0 && distance < 10) {
+            radio.sendString("Too close!")
+        }  
+    }
+})
