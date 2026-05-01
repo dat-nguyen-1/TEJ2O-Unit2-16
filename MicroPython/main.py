1704c7e64cedbf16a9f5932407e39a63b9e122e1
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-05-01 17:35:29 -0400

    Update main.py

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,31 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Dat Nguyen
+Created on: Apr 2026
+This module will recieve radio messages
 """
 
 from microbit import *
+import radio
 
-display.scroll("Hello, World!")
+
+# initialize display
+display.clear()
+display.show(Image.HAPPY)
+
+# initialize radio
+radio.on()
+radio.config(group=153)
+
+# main loop
+while True:
+    # get message
+    message = radio.receive()
+
+    # check if message is not None
+    if message is not None:
+        # display message
+        display.clear()
+        display.scroll(str(message))
+
+    # reset display
+    display.show(Image.HAPPY)
